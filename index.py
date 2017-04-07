52ff68d238b5950e95b5702e9c034c44c67af0f0
hayaken8112 <hayashi8112@gmail.com>
2017-04-07 16:28:31 +0900

    post_test

--- index.py
+++ index.py
@@ -1,13 +1,19 @@
-from bottle import route, run, template, request, static_file, HTTPResponse
+from bottle import route, run, template, request, static_file, HTTPResponse 
 import os
 import json
 
 @route('/ping', method='GET')
 def ping():
-  body = {"res":"pong"}
-  r = HTTPResponse(status=200, body=json.dumps(body))
-  r.set_header('Content-Type', 'application/json')
-  return r
+    body = {"res":"pong"}
+    r = HTTPResponse(status=200, body=json.dumps(body))
+    r.set_header('Content-Type', 'application/json')
+    return r
+
+@route('/ping', method='POST')
+def post():
+    body = request.forms()
+    r = HTTPResponse(status=200, body=body)
+    return r
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
