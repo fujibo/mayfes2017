80ee048b03924a405f951a69aaaf7a9a54a6e385
417 <s417.lama@gmail.com>
2017-04-07 15:55:12 +0900

    画像の切り出しを行う関数setImageを追加しました。画像ファイルは含まれていません。

--- WebGUI/js/script.js
+++ WebGUI/js/script.js
@@ -6,7 +6,6 @@ $(function() {
     var borderWidth = 1;
     var canvasX = $("#canvas").offset().left + borderWidth; 
     var canvasY = $("#canvas").offset().top + borderWidth;
-    console.log("x: " + canvasX + ", y: " + canvasY);
 
     var prevX = null;
     var prevY = null;
@@ -38,7 +37,26 @@ $(function() {
             ctx.stroke();
             prevX = x;
             prevY = y;
-            console.log("x: " + x + ", y: " + y);
         }
     });
+    setImage(1, "sample.jpg", 200, 200, 1000, 1000);
+    setImage(2, "sample.jpg", 300, 400, 600, 700);
+    setImage(3, "sample.jpg", 600, 600, 700, 700);
+
+    function setImage(num, path, left, top, right, bottom) {
+        // size in html
+        var width = $("#image" + num).width();
+        var height = $("#image" + num).height();
+        var selector = "#image" + num + " img";
+        $(selector).attr("src", "image/" + path);
+        // original size of the image
+        var origWidth = $(selector).width();
+        var origHeight = $(selector).height();
+
+        var ratio = width / (right - left);
+        $(selector)
+        .width(origWidth * ratio)
+        .css("left", "-" + (left * ratio) + "px")
+        .css("top", "-" + (top * ratio) + "px");
+    }
 });
